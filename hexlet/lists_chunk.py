d262171b5389a08b7d620b4d17269d9335c773ea
KAnanev <russrage@gmail.com>
2021-01-28 23:43:56 +0300

    add lists_chunk.py v. 2

--- hexlet/lists_chunk.py
+++ hexlet/lists_chunk.py
@@ -1,5 +1,11 @@
+# def chunked(num, data):
+#     return [data[i:i + num] for i in range(0, len(data), num)]
+
 def chunked(num, data):
-    return [data[i:i + num] for i in range(0, len(data), num)]
+    list_data = []
+    for i in range(0, len(data), num):
+        list_data.append(data[i:i + num])
+    return list_data
 
 
 print(chunked(2, ('A', 'B', 'C', 'D')))
