42d3032e65b5576acab136a4d362b502a737eec2
KAnanev <russrage@gmail.com>
2021-01-29 17:36:52 +0300

    add lists_longest_substring.py

--- hexlet/lists_longest_substring.py
+++ hexlet/lists_longest_substring.py
@@ -9,6 +9,17 @@ def find_longest_length(string):
         longest = max(longest, len(contains))
     return longest
 
+# def find_longest_length(string):
+#     ptr, max_len = 0, 0
+#     store = {}
+#     for (i, ch) in enumerate(string):
+#         if ch not in store:
+#             store[ch] = i
+#         else:
+#             max_len = max(max_len, i - ptr)
+#             ptr = store[ch] + 1
+#     return max(max_len, len(string) - ptr)
+
 
 assert find_longest_length('') == 0
 assert find_longest_length('a') == 1
